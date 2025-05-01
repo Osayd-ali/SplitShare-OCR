a8354498254844e7dbd244b1de8ef5e72cfa7e76
Alexander Lazarov <alzlaz.learns@gmail.com>
2025-05-01 10:45:16 -0500

    update: change folder structure to be more appropriate and added sample HelloController to show sprringboot is working properly and added corresponding test. also created Dockerfile as presetup for tesseract libraries & Dockerfile.test to run test seperately.

--- src/main/java/com/splitshare/splitshare/SplitshareApplication.java
+++ src/main/java/com/splitshare/splitshare/SplitshareApplication.java
@@ -1,4 +1,4 @@
-package _1.splitshare.splitshare;
+package com.splitshare.splitshare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/splitshare/splitshare/SplitshareApplicationTests.java
+++ src/test/java/com/splitshare/splitshare/SplitshareApplicationTests.java
@@ -1,4 +1,4 @@
-package _1.splitshare.splitshare;
+package com.splitshare.splitshare;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
